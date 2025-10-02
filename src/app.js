061d75b58edb6cfed3d13b1065bf80a6ee60b1b4
Yukesh-30 <yukesh2310573@ssn.edu.in>
2025-10-02 22:00:27 +0530

    feat:added middleware for not found error

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import productRoutes from './routes/product.routes.js';
 import seedDB from '../scripts/seed.js';
+import notFound from './middlewares/notFound.middleware.js';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send("Welcome to Homepage");
 })
+
+app.use(notFound)
 app.use('/api',productRoutes);
 export default app
 
